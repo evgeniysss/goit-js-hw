14e13b681e3a29cbe0ecf7d438abcad7070ce584
kievtechnoshop@gmail.com <swr83947>
2019-10-10 14:47:36 +0300

    Module-4

--- goit-js-hw-02/js/task-5.js
+++ goit-js-hw-02/js/task-5.js
@@ -35,6 +35,21 @@ console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
 
 
 
+//  SECOND VERSION
+// let string = "";
+// const word = "spam";
+// const word1 = "sale";
+// const checkForSpam = function(string) {
+//  return (
+//    string.toLowerCase().includes(word) || string.toLowerCase().includes(word1)
+//  );
+// };
+// console.log(checkForSpam("Latest technology news")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
 
 
 
---
+++ goit-js-hw-04/js/task-2.js
@@ -0,0 +1,31 @@
+// Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+
+'use strict';
+const inventory = {
+  items: ['Монорельса', 'Фильтр'],
+  add(itemName) {
+    console.log(itemName);
+    console.log(inventory.items);
+    console.log(this);
+    this.items.push(itemName);
+
+  },
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking ${action.name} opeartion on ${itemName}`);
+  action(itemName);
+};
+
+invokeInventoryAction('Аптечка', inventory.add.bind(inventory));
+// Invoking add opeartion on Аптечка
+
+console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+invokeInventoryAction('Фильтр', inventory.remove.bind(inventory));
+// Invoking remove opeartion on Фильтр
+
+console.log(inventory.items); // ['Монорельса', 'Аптечка']
